79401371ac026d04c6b4aa1b250b075464f8dbc2
Alec <frstrtr@gmail.com>
2023-09-01 08:10:24 +0400

    playing with different address types

--- bitcoin_multisig.py
+++ bitcoin_multisig.py
@@ -4,8 +4,23 @@ from bitcoin import *
 
 def generate_keypair():
     private_key = os.urandom(32).hex()
+    wif_key = encode_privkey(private_key, "wif_compressed")
     public_key = privtopub(private_key)
-    return private_key, public_key
+    compressed_pub = compress(public_key)
+    uncompressed_address = pubtoaddr(public_key)
+    compressed_address = pubtoaddr(compressed_pub)
+    uncompressed_p2pk_address = pubkey_to_address(public_key)
+    compressed_p2pk_address = pubkey_to_address(compressed_pub)
+    return (
+        private_key,
+        wif_key,
+        public_key,
+        compressed_pub,
+        uncompressed_address,
+        compressed_address,
+        uncompressed_p2pk_address,
+        compressed_p2pk_address,
+    )
 
 
 def m_of_n_multisig_wallet():
@@ -15,15 +30,33 @@ def m_of_n_multisig_wallet():
     if m > n:
         raise ValueError("'m' should be less than or equal to 'n'")
 
-    public_keys = []
-
-    for _ in range(n):
-        _, pub_key = generate_keypair()
-        public_keys.append(pub_key)
+    keys_data = [generate_keypair() for _ in range(n)]
+    public_keys = [data[2] for data in keys_data]
 
     # Create a multisig P2SH address using the `mk_multisig_script` function
     multisig_script = mk_multisig_script(public_keys, m, n)
     address = scriptaddr(multisig_script)
+
+    print("\nGenerated Keys:")
+    for i, (
+        priv,
+        wif,
+        pub,
+        compressed_pub,
+        uncompressed_addr,
+        compressed_addr,
+        uncompressed_p2pk_addr,
+        compressed_p2pk_addr,
+    ) in enumerate(keys_data, 1):
+        print(f"\nKeypair {i}:")
+        print(f"Private Key (HEX): {priv}")
+        print(f"Private Key (WIF): {wif}")
+        print(f"Public Key: {pub}")
+        print(f"Compressed Public Key: {compressed_pub}")
+        print(f"Bitcoin Address (Uncompressed): {uncompressed_addr}")
+        print(f"Bitcoin Address (Compressed): {compressed_addr}")
+        print(f"Bitcoin P2PK Address (Uncompressed): {uncompressed_p2pk_addr}")
+        print(f"Bitcoin P2PK Address (Compressed): {compressed_p2pk_addr}")
 
     print(f"\nYour {m} of {n} multisig P2SH address is: {address}")
 
