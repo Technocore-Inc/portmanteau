c1b109439c8059e5b0e0b69dd7b83ffec9bf8110
Alec <frstrtr@gmail.com>
2023-09-01 08:23:48 +0400

    address prefixes comments

--- bitcoin_multisig.py
+++ bitcoin_multisig.py
@@ -63,3 +63,17 @@ def m_of_n_multisig_wallet():
 
 if __name__ == "__main__":
     m_of_n_multisig_wallet()
+
+# P2PKH = The hash160 of a compressed public key.
+# P2PK = A compressed public key (even if the original script contained an uncompressed public key). A compressed public key always starts with a 02 or 03 to indicate whether the y value is even or odd. However, if the original script contained an uncompressed public key, it gets compressed when storing it in the chainstate database to save space. We then use an 04 or 05 to indicate that it was originally an uncompressed key and to also indicate whether the original y was even or odd.
+# P2SH = The hash160 of a script.
+# Everything else = The full scriptpubkey.
+# P2WPKH = Always starts with a 0014, followed by the hash160 of a public key.
+# P2WSH = Always starts with a 0020, followed by the sha256 hash of a script.
+# 00  = P2PKH <- upcoming data is the hash160 public key
+# 01  = P2SH  <- upcoming data is the hash160 of a script
+# 02  = P2PK  <- upcoming data is a compressed public key (nsize makes up part of the public key) [y=even]
+# 03  = P2PK  <- upcoming data is a compressed public key (nsize makes up part of the public key) [y=odd]
+# 04  = P2PK  <- upcoming data is an uncompressed public key (but has been compressed for leveldb) [y=even]
+# 05  = P2PK  <- upcoming data is an uncompressed public key (but has been compressed for leveldb) [y=odd]
+# 06+ =       <- indicates size of upcoming full script (subtract 6 to get the actual size in bytes)
